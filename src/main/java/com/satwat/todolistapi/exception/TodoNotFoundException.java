173b3700aad867da8de8701e7afbbd0d3a6c6ba0
Satwat Kr Ambashta <satwatkr@gmail.com>
2025-07-05 01:56:22 +0530

    discard repo

--- src/main/java/com/satwat/todolistapi/exception/TodoNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.satwat.todolistapi.exception;
-
-public class TodoNotFoundException extends RuntimeException{
-    public TodoNotFoundException(Long id){
-        super("Todo not found with id: "+ id);
-    }
-}
--- src/main/java/com/satwat/todolistapi/service/TodoService.java
+++
@@ -1,8 +0,0 @@
-package com.satwat.todolistapi.service;
-import com.satwat.todolistapi.models.Todo;
-import java.util.List;
-
-public interface TodoService {
-    List<Todo> getAllTodos ();
-    List<Todo> getTodo (Long id);
-}
